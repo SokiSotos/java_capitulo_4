26de02c5de8646575be712498f11be0906b7090b
SokiSotos <sokisotos@hotmail.com>
2022-10-14 01:39:21 +0200

    Ej2 - Hora

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Ejercicio2 {
+public class Ejercicio2{
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce una hora");
