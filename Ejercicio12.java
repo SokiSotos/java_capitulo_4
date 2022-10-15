b52091dbcb7f1cf2164933db9d550264fd78ed63
SokiSotos <sokisotos@hotmail.com>
2022-10-15 21:11:00 +0200

    Ej12 - Cuestionario

---
+++ Ejercicio12.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+public class Ejercicio12{
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int nota=0;
+        char res;
+        System.out.println("Pregunta 1");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+
+        System.out.println("Pregunta 2");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+
+        System.out.println("Pregunta 3");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+
+        System.out.println("Pregunta 4");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+        System.out.println("Pregunta 5");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+
+        System.out.println("Pregunta 6");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+
+        System.out.println("Pregunta 7");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+
+        System.out.println("Pregunta 8");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+         System.out.println("Pregunta 9");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+
+        System.out.println("Pregunta 10");
+        System.out.println("¿Que es un platano?");
+        System.out.println("a. Una fruta");
+        System.out.println("b. Un mueble");
+        System.out.println("c. un vehiculo");
+        res = sc.next().charAt(0);
+            if(res=='a'){
+                nota++;
+                System.out.println("Es correcto\n");
+            }
+            else{
+                System.out.println("Es Incorrecto\n");
+            }
+                       
+        System.out.println("Ha conseguido un total de " + nota + " Puntos");
+    sc.close();
+    }
+}
